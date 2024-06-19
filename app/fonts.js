21eaa6a1fe50f893c6a33cad2e8db83dbab7558a
fgrreloaded <nk.nitish.nk500@gmail.com>
2024-06-19 09:19:20 +0530

    init: Setting up project, adding fonts, Creating Navbar

---
+++ app/fonts.js
@@ -0,0 +1,15 @@
+import {Montserrat, Play} from 'next/font/google'
+
+
+
+
+export const montserrat = Montserrat({
+    subsets: ['latin'],
+    display: 'swap',
+})
+
+export const play = Play({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: "400"
+})
